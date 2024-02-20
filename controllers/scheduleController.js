fa0d9bda49507b08c47d6e8c563418b4e1afda99
Joel Komieter <komieterj@gmail.com>
2024-02-20 04:03:46 -0500

    user can now add schedules.
    Including time functionalities, formatting.

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -1,20 +1,20 @@
 const { auth, db } = require("../config/firebase");
-const { doc, setDoc } = require("firebase/firestore");
+const { doc, addDoc, collection, getDocs, getDoc } = require("firebase/firestore");
 
 module.exports.addSchedule = async (req, res) => {
     try {
-        const { userId, title, date, start_time, stop_time, hasStarted, course } = req.body;
+        const { userId, title, date, start_time, stop_time, scheduleType } = req.body;
         const userDoc = doc(db, "users", userId);
-        await setDoc(userDoc, {
+        const scheduleCollection = collection(userDoc, "schedules")
+        await addDoc(scheduleCollection, {
             title,
             date,
             start_time,
             stop_time,
-            hasStarted,
-            course
-        });
+            scheduleType
+        })
 
-        res.status(200).json({message: "Successfully added schedule"});
+        res.status(200).json({ message: "Successfully added schedule" });
     } catch (error) {
         console.log(error);
         res.status(400).json({ error: "Something went wrong" })
@@ -23,4 +23,21 @@ module.exports.addSchedule = async (req, res) => {
 
 module.exports.updateSchedule = async (req, res) => {
 
+}
+
+module.exports.getSchedules = async (req, res) => {
+    console.log("fetching....")
+    try {
+        const {userId} = req.query;
+        const userCollection = collection(db, 'users'); 
+        const userDoc = doc(userCollection, userId);
+        const scheduleCollection = collection(userDoc, 'schedules');
+        const schedulesSnapShot = await getDocs(scheduleCollection);
+        const schedules = schedulesSnapShot.docs.map((d) => d.data());
+        console.log(schedules)
+        res.json(schedules)
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ error: error.message })
+    }
 }
